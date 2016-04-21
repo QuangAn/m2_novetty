56cd09cd12774fd4caa058451154f0466eacdaa2
Hanh Phan <phanngochanh.pnh@gmail.com>
2016-04-21 16:16:02 +0700

    abc

--- app/design/frontend/MT/clevertheme/web/js/jsclevertheme.js
+++ app/design/frontend/MT/clevertheme/web/js/jsclevertheme.js
@@ -1,4 +1,7 @@
  jQuery(function () {
     jQuery.material.init(); 
- 
+    jQuery.material.ripples(".owl-nav div");
+    jQuery.material.ripples(".actions-primary");
+    jQuery.material.ripples(".mt-actions-secondary a");
+     
   }); 
